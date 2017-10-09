cc94e714d46b28c0b077f52d7ea9824e1717b716
Anmol Nuggehalli <anuggehalli93@gmail.com>
2017-10-08 19:20:26 -0600

    main.js

---
+++ flashcards.js
@@ -0,0 +1,36 @@
+// exports.cards = function(front, back){
+//     this.front_card = front;
+//     this.back_card = back
+// };
+
+// exports.ClozedCards = function(text, close){
+//     var lower_text = text.toLowerCase();
+//     var upper_text = cloze.toUpperCase();
+
+    
+//     if (!lower_text.include(cloze.toUpperCase)){
+        
+//     }
+
+// };
+var basicCard = require('basicCard.js');
+var Pres1 = new basicCard('who was the 1st POTUS?', Washington);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
